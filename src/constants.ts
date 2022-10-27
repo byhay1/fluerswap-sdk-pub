6a25c62d8e8788f357786982b7b5a0325aa1c6cc
BeeSting(Byron) <byhay1@terpmail.umd.edu>
2022-10-27 00:19:08 -0400

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,58 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 42262,
+  TESTNET = 42261,
+  SAPPHIRE = 23295
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0x4f51Af4988C67cC37FBA64898AF239FA4e2Bea22',
+  [ChainId.TESTNET]: '0x45c0977C125f133fA2F637183F0c3886830587D0',
+  [ChainId.SAPPHIRE]: '0x0000000000000000000000000000000000000001'
+}
+
+export const INIT_CODE_HASH = '0x32a54259978659d9843f373896c5cae62ec46ca33ca45bdf79e1e56409a79bf8'
+
+export const INIT_CODE_HASH_MAP = {
+  [ChainId.MAINNET]: INIT_CODE_HASH,
+  [ChainId.TESTNET]: INIT_CODE_HASH,
+  [ChainId.SAPPHIRE]: INIT_CODE_HASH
+}
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
